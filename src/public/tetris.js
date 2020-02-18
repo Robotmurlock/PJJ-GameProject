7acac784004c3618d97a49ddf64440b1c0d95aa6
Robotmurlock <momir.adzemovic@gmail.com>
2020-02-18 23:52:33 +0100

    Refactoring code

--- src/public/tetris.js
+++ src/public/tetris.js
@@ -2,8 +2,7 @@ const game_canvas = document.getElementById("tetris");
 const game_context = game_canvas.getContext("2d");
 
 // board constants
-const NUMBER_OF_ROWS = 20;
-const NUMBER_OF_COLUMNS = 20;
+const SIZE = 20;
 const BLOCK_SIZE = 30;
 const BLOCK_EMPTY_COLOR = "white";
 const BLOCK_DEAD_COLOR = "gray";
@@ -28,8 +27,8 @@ const draw_square = function(row, col, color) {
 }
 
 const draw_board = function() {
-    for(let row=0; row<NUMBER_OF_ROWS; row++) {
-        for(let col=0; col<NUMBER_OF_COLUMNS; col++) {
+    for(let row=0; row<SIZE; row++) {
+        for(let col=0; col<SIZE; col++) {
             draw_square(row, col, game_board[row][col])
         }
     }
@@ -37,7 +36,7 @@ const draw_board = function() {
 
 const layer_blackout = function() {
     let index = game_layer_to_destroy_index;
-    for(let col=0; col<NUMBER_OF_COLUMNS; col++) {
+    for(let col=0; col<SIZE; col++) {
         game_board[col][index] = BLOCK_DEAD_COLOR;
         draw_square(col, index, BLOCK_DEAD_COLOR);
     }
@@ -46,7 +45,7 @@ const layer_blackout = function() {
 const destroy_board_layer = function() {
     let index = game_layer_to_destroy_index;
     for(let layer=index; layer>0; layer--) {
-        for(let col=0; col<NUMBER_OF_COLUMNS; col++) {
+        for(let col=0; col<SIZE; col++) {
             game_board[col][layer] = game_board[col][layer-1];
             game_row_block_count[layer] = game_row_block_count[layer-1];
         }
@@ -69,7 +68,7 @@ Piece.prototype.generate = function() {
     this.type = types[Math.floor(Math.random() * types.length)];
     this.size_x = this.type[0].length
     this.size_y = this.type[0][0].length
-    this.pos_x = Math.floor(Math.random() * (NUMBER_OF_COLUMNS-this.size_x));
+    this.pos_x = Math.floor(Math.random() * (SIZE-this.size_x));
     this.pos_y = 0;
     this.state = 0;
 
@@ -91,9 +90,9 @@ const valid_position = function(x, y) {
         return false;
     if(y < 0)
         return false;
-    if(x >= NUMBER_OF_COLUMNS)
+    if(x >= SIZE)
         return false;
-    if(y >= NUMBER_OF_ROWS)
+    if(y >= SIZE)
         return false;
     return true;
 }
@@ -115,10 +114,10 @@ const max = function(a, b) {
 }
 
 Piece.prototype.check_height = function() {
-    for(let x=0; x<NUMBER_OF_ROWS; x++) {
-        for(let y=0; y<NUMBER_OF_COLUMNS; y++) {
+    for(let x=0; x<SIZE; x++) {
+        for(let y=0; y<SIZE; y++) {
             if(game_board[y][x] != BLOCK_EMPTY_COLOR) {
-                return (NUMBER_OF_COLUMNS - x);
+                return (SIZE - x);
             }
         }
     }
@@ -128,7 +127,7 @@ Piece.prototype.restart = function() {
     clearInterval(this.timer);
 
     let height = this.check_height();
-    if(height >= NUMBER_OF_COLUMNS-5) {
+    if(height >= SIZE-5) {
         game_over = true;
         console.log("Game Over!");
 
@@ -148,7 +147,7 @@ Piece.prototype.restart = function() {
         for(let y=0; y<this.size_y; y++) {
             if(this.type[this.state][x][y] == 1) {
                 game_row_block_count[this.pos_y+y]++;
-                if(game_row_block_count[this.pos_y+y] >= NUMBER_OF_COLUMNS) {
+                if(game_row_block_count[this.pos_y+y] >= SIZE) {
                     game_layer_to_destroy_index = this.pos_y+y;
 
                     game_score++;
@@ -181,7 +180,7 @@ Piece.prototype.collision = function() {
             }
     }
 
-    if(this.pos_y + max_bottom + 1 >= NUMBER_OF_ROWS)
+    if(this.pos_y + max_bottom + 1 >= SIZE)
         return true;
 
     for(let i=0; i<this.size_x; i++) {
@@ -190,8 +189,8 @@ Piece.prototype.collision = function() {
             continue;
         if(this.pos_x+i >= 0
             && this.pos_y + b + 1 >= 0
-            && this.pos_x+i < NUMBER_OF_ROWS 
-            && this.pos_y + b + 1 < NUMBER_OF_COLUMNS
+            && this.pos_x+i < SIZE 
+            && this.pos_y + b + 1 < SIZE
             && game_board[this.pos_x+i][this.pos_y + b + 1] != BLOCK_EMPTY_COLOR)
             return true;
     }
@@ -271,10 +270,10 @@ Piece.prototype.falling = function() {
 const start = function() {
     // set default block colors and row block count to zero
     game_over = false;
-    for(let row=0; row<NUMBER_OF_ROWS; row++) {
+    for(let row=0; row<SIZE; row++) {
         game_board[row] = [];
         game_row_block_count[row] = 0;
-        for(let col=0; col<NUMBER_OF_COLUMNS; col++) {
+        for(let col=0; col<SIZE; col++) {
             game_board[row][col] = BLOCK_EMPTY_COLOR;
         }
     }
